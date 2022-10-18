499a384eb09946f448d28df8b5c34b599b6d0557
João Marcos Lacerda dos Santos <104016774+joaomarcosls@users.noreply.github.com>
2022-10-17 21:08:34 -0300

    v 1.0.0

--- assistente_ibm.py
+++ assistente_ibm.py
@@ -1,4 +1,4 @@
-import speech_recognition as sr
+from ibm import *
 from nltk import word_tokenize, corpus
 import json
 
@@ -8,12 +8,10 @@ CAMINHO_CONFIGURACAO = "config.json"
 
 
 def iniciar():
-    global reconhecedor
     global palavras_de_parada
     global nome_assistente
     global acoes
 
-    reconhecedor = sr.Recognizer()
     palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
 
     with open(CAMINHO_CONFIGURACAO, "r") as arquivo_configuracao:
@@ -26,71 +24,66 @@ def iniciar():
 
 
 def escutar_comando():
-    global reconhecedor
-    
     comando = None
-    
-    with sr.Microphone() as fonte_audio:
-        reconhecedor.adjust_for_ambient_noise(fonte_audio)
-        
-        print("Fale alguma coisa...")        
-        fala = reconhecedor.listen(fonte_audio, timeout=5, phrase_time_limit=5)
-        try:
-            comando = reconhecedor.recognize_google(fala, language=IDIOMA_FALA)
-        except sr.UnknownValueError:
-            pass
-    
+
+    configurar_servico()
+    audio_capturado, audio = capturar_audio()
+    if audio_capturado:
+        confianca, texto = reconhecer_texto(audio)
+        if confianca > 0.70:
+            comando = texto
+
     return comando
-    
 
 
 def eliminar_palavras_de_parada(tokens):
     global palavras_de_parada
-    
+
     tokens_filtrados = []
     for token in tokens:
         if token not in palavras_de_parada:
             tokens_filtrados.append(token)
-    
+
     return tokens_filtrados
 
 
 def tokenizar_comando(comando):
     global nome_assistente
-    
+
     acao = None
     objeto = None
-    
+
     tokens = word_tokenize(comando, IDIOMA_CORPUS)
     if tokens:
         tokens = eliminar_palavras_de_parada(tokens)
-        
+
         if len(tokens) >= 3:
             if nome_assistente == tokens[0].lower():
                 acao = tokens[1].lower()
-                objeto = tokens[2].lower()            
-    
+                objeto = tokens[2].lower()
+
     return acao, objeto
-    
+
 
 def validar_comando(acao, objeto):
     global acoes
-    
+
     valido = False
-    
+
     if acao and objeto:
         for acaoCadastrada in acoes:
             if acao == acaoCadastrada["nome"]:
                 if objeto in acaoCadastrada["objetos"]:
                     valido = True
-                    
+
                 break
-    
+
     return valido
 
 
 def executar_comando(acao, objeto):
     print("vou executar o comando:", acao, objeto)
+
 
 if __name__ == '__main__':
     iniciar()
@@ -99,9 +92,9 @@ if __name__ == '__main__':
     while continuar:
         try:
             comando = escutar_comando()
-            print(f"processando o comando: {comando}")
-
             if comando:
+                print(f"processando o comando: {comando}")
+
                 acao, objeto = tokenizar_comando(comando)
                 valido = validar_comando(acao, objeto)
                 if valido:
